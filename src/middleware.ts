cd991ddf4fabf73cd763e7c8c46bf27df882df0d
kage1020 <4620087@ed.tus.ac.jp>
2024-08-30 10:15:02 +0900

    add status code queries

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,20 @@
-import type { NextRequest } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 
 export default function middleware(req: NextRequest) {
   const query = req.nextUrl.searchParams.get("q")
 
-  if (query === "403") {
-    return new Response("Forbidden", { status: 403 })
+  switch (query) {
+    case "400":
+      return new NextResponse("Bad Request", { status: 400 })
+    case "401":
+      return new NextResponse("Unauthorized", { status: 401 })
+    case "403":
+      return new NextResponse("Forbidden", { status: 403 })
+    case "404":
+      return new NextResponse("Not Found", { status: 404 })
+    case "418":
+      return new NextResponse("I'm a teapot", { status: 418 })
+    case "500":
+      return new NextResponse("Internal Server Error", { status: 500 })
   }
 }
